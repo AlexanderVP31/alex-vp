8adc8c5c0329ca8a204465f6f2ae595b1523e861
AlexanderVP31 <alexvalverde2031@gmail.com>
2025-06-16 10:34:04 -0500

    cambios en mi package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/alex-vp/", 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/alex-vp', 
 });
