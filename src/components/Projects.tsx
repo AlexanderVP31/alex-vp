e0e4fd7ebd78e52f67c0e5a57b02378b539a3ec9
AlexanderVP31 <alexvalverde2031@gmail.com>
2025-06-17 14:02:01 -0500

    subiendo videos faltantes de mis proyectos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,14 +11,14 @@ import { useTranslation } from '../hooks/useTranslation';
 import ReactDOM from 'react-dom';
 
 // Importa tus assets de video aquí
-//import DocumentoDigitalVideo from '../assets/video/DocumentoDigital.mp4';
+import AdminDashboards from '../assets/video/AdminDashboards.mp4';
 import DocumentoDigitalVideo from '../assets/video/DocumentoDigital.mp4';
-import OePromartVideo from '../assets/video/OePromartVideo.mp4';
+//import OePromartVideo from '../assets/video/OePromartVideo.mp4';
 import SeguroVideo from '../assets/video/SeguroVideo.mp4';
 import MowaVideo from '../assets/video/MowaVideo.mp4';
+//import MowaVideo from 'https://www.youtube.com/watch?v=o03dByzxbXo';
 
-
-import AdminDashboards from '../assets/img/adminDashboards.jpg';
+//import AdminDashboards from '../assets/img/adminDashboards.jpg';
 
 // Componente de Video con controles personalizados
 const CustomVideoPlayer = ({ src, title, onFullscreen }) => {
@@ -366,17 +366,17 @@ const Projects: React.FC = () => {
       link: 'https://actours.com.pe/adminproveedor/login',
       github: 'https://github.com/avmkui/app_proveedor',
     },
+    // {
+    //   id: 3,
+    //   title: t('oepromarPlatform'),
+    //   description: t('oepromarDesc'),
+    //   video: OePromartVideo,
+    //   tags: ['Angular','PrimeNG', 'Java','Spring Boot', 'Sql Server'],
+    //   link: 'https://actours.com.pe/adminproveedor/login',
+    //   github: 'https://github.com/avmkui/app_proveedor',
+    // },
     {
       id: 3,
-      title: t('oepromarPlatform'),
-      description: t('oepromarDesc'),
-      video: OePromartVideo,
-      tags: ['Angular','PrimeNG', 'Java','Spring Boot', 'Sql Server'],
-      link: 'https://actours.com.pe/adminproveedor/login',
-      github: 'https://github.com/avmkui/app_proveedor',
-    },
-    {
-      id: 4,
       title: t('seguroPlatform'),
       description: t('seguroDesc'),
       video: SeguroVideo,
@@ -385,7 +385,7 @@ const Projects: React.FC = () => {
       github: 'https://github.com/avmkui/app_proveedor',
     },
     {
-      id: 5,
+      id: 4,
       title: t('mowaPlatform'),
       description: t('mowaDesc'),
       video: MowaVideo,
@@ -394,7 +394,7 @@ const Projects: React.FC = () => {
       github: 'https://github.com/avmkui/app_proveedor',
     },
     {
-      id: 6,
+      id: 5,
       title: t('ecommercePlatform'),
       description: t('ecommerceDesc'),
       image: 'https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -403,7 +403,7 @@ const Projects: React.FC = () => {
       github: '#',
     },
     {
-      id: 7,
+      id: 6,
       title: t('musicStreamingApp'),
       description: t('musicStreamingDesc'),
       image: 'https://images.pexels.com/photos/3756766/pexels-photo-3756766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -412,7 +412,7 @@ const Projects: React.FC = () => {
       github: '#',
     },
     {
-      id: 8,
+      id: 7,
       title: t('aiContentGenerator'),
       description: t('aiContentDesc'),
       image: 'https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -421,7 +421,7 @@ const Projects: React.FC = () => {
       github: '#',
     },
     {
-      id: 9,
+      id: 8,
       title: t('portfolioWebsite'),
       description: t('portfolioDesc'),
       image: 'https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
