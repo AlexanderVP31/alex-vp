cbaae407285f59e4140a584f40f8bfb73117e10a
AlexDevs <71418618+AlexanderVP31@users.noreply.github.com>
2025-10-21 14:50:32 -0500

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/alex-vp/", 
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/alex-vp/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
