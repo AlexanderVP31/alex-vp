6c0a6ce8cbd17c929c84e41f2e8f8a73f8a15760
AlexDevs <71418618+AlexanderVP31@users.noreply.github.com>
2026-03-16 10:49:21 -0500

    Update vite.config.ts
    CAMBIOS EN EL BUILD
    
    Signed-off-by: AlexDevs <71418618+AlexanderVP31@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/alex-vp/' : '/',
+export default defineConfig({
+  base: "/alex-vp/",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-}));
+});
